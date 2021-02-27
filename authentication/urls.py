b87e07521a42b467c390aa3a5264e6c2d0995ecd
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-02-27 16:24:25 +0530

    URL setup

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-"""Blog URL Configuration
+"""authentication URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/',include('authentication.urls')),
-    path('',include('blog.urls')),
+    
 ]
