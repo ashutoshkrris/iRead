1aab78cae72184753351fa81b51350b5d38cbd4e
PrabaKaran <83495114+Nikolatesla2001@users.noreply.github.com>
2021-07-13 23:52:52 -0700

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
+    bottom: '32px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'Unset', // default: 'unset'
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
