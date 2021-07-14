ebc5503785784eb423e28b1d64047f61cb2bf409
PrabaKaran <83495114+Nikolatesla2001@users.noreply.github.com>
2021-07-14 12:04:13 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
