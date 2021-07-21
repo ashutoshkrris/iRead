cb0a04c80ae1999daf89ae909985fe683cf0f2d4
mayank-wadhwa-git <68892762+mayank-wadhwa-git@users.noreply.github.com>
2021-07-21 15:54:32 +0530

    Added Scroll Bar

--- Blog/settings.py
+++ Blog/settings.py
@@ -110,6 +110,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {    
+#     'default': {        
+#         'ENGINE': 'django.db.backends.sqlite3',        
+#         'NAME': BASE_DIR / 'db.sqlite3',    
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
