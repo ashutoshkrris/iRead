e6d3154edf6344565d7b808b61346a713561b602
Ashutosh Krishna <ashutoshbritish@gmail.com>
2022-04-04 09:58:17 +0530

    Encode Audio Base64

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,5 @@
-import cloudinary
-from django.conf import settings
 import html
 from django.utils.html import strip_tags
-import os
 from decouple import config
 import requests
 
@@ -10,21 +7,16 @@ BASE_URL = "http://api.voicerss.org/"
 
 
 def convert_to_audio(post):
-    content = strip_tags(html.unescape(post.content))
-
-    if settings.DEBUG:
-        if not os.path.exists(f"{settings.MEDIA_ROOT}/blog/audio/"):
-            os.mkdir(f"{settings.MEDIA_ROOT}/blog/audio/")
-        filename = f"{settings.MEDIA_ROOT}/blog/audio/audio_{post.id}.wav"
-    else:
-        filename = f"{settings.TEMP_MEDIA_DIR}/audio_{post.id}.wav"
+    intro = f'{post.title} written by {post.author.get_full_name()}\n'
+    content = intro + strip_tags(html.unescape(post.content))
 
     params = {
         'key': config("VOICERSS_API"),
         'hl': 'en-us',
         'v': 'Mike',
         'c': 'wav',
-        'f': '8khz_8bit_mono'
+        'f': '8khz_8bit_mono',
+        'b64': True
     }
 
     data = {
@@ -34,24 +26,5 @@ def convert_to_audio(post):
     response = requests.post(BASE_URL, data=data, params=params)
 
     if response.status_code == 200:
-        with open(filename, "bx") as f:
-            f.write(response.content)
-
-    post.audio_url = f"{settings.MEDIA_URL}blog/audio/audio_{post.id}.wav"
-    if not settings.DEBUG:
-        response = upload_to_cloudinary(filename, post)
-        post.audio_url = response.get('secure_url')
-    post.save()
-
-
-def upload_to_cloudinary(audio_file, post):
-    cloudinary.config(
-        cloud_name=config("CLOUD_NAME"),
-        api_key=config("API_KEY"),
-        api_secret=config("API_SECRET"),
-        secure=True
-    )
-    response = cloudinary.uploader.upload(
-        audio_file, public_id=f"audio_{post.id}", folder="media/blog/audio/", resource_type="video")
-    os.remove(audio_file)
-    return response
+        post.audio_data = response.content.decode('utf-8')
+        post.save()
